69b2d32ef74402ef0c470437c9cd2f7c62bd4df9
Al Stone <ahs3@ahs3.net>
2018-05-14 15:56:39 -0600

    New upstream version 20180508

--- source/components/executer/exconfig.c
+++ source/components/executer/exconfig.c
@@ -233,6 +233,11 @@ AcpiExLoadTableOp (
         return_ACPI_STATUS (Status);
     }
 
+    /* Complete the initialization/resolution of package objects */
+
+    Status = AcpiNsWalkNamespace (ACPI_TYPE_PACKAGE, ACPI_ROOT_OBJECT,
+        ACPI_UINT32_MAX, 0, AcpiNsInitOnePackage, NULL, NULL, NULL);
+
     /* Parameter Data (optional) */
 
     if (ParameterNode)
@@ -505,6 +510,11 @@ AcpiExLoadOp (
 
         return_ACPI_STATUS (Status);
     }
+
+    /* Complete the initialization/resolution of package objects */
+
+    Status = AcpiNsWalkNamespace (ACPI_TYPE_PACKAGE, ACPI_ROOT_OBJECT,
+        ACPI_UINT32_MAX, 0, AcpiNsInitOnePackage, NULL, NULL, NULL);
 
     /* Store the DdbHandle into the Target operand */
 
