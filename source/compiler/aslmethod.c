38770898bccfdac5eef19442d211e5164dfdf3a1
Al Stone <ahs3@ahs3.net>
2020-09-13 15:11:30 -0600

    New upstream version 20200717

--- source/compiler/aslmethod.c
+++ source/compiler/aslmethod.c
@@ -198,6 +198,8 @@ MtMethodAnalysisWalkBegin (
         {
             ActualArgs = MtProcessParameterTypeList (NextType,
                 MethodInfo->ValidArgTypes);
+            MethodInfo->NumArguments = ActualArgs;
+            ArgNode->Asl.Value.Integer |= ActualArgs;
         }
 
         if ((MethodInfo->NumArguments) &&
@@ -562,6 +564,16 @@ MtProcessParameterTypeList (
 {
     UINT8                   ParameterCount = 0;
 
+
+    if (ParamTypeOp && ParamTypeOp->Asl.ParseOpcode != PARSEOP_DEFAULT_ARG)
+    {
+        /* Special case for a single parameter without braces */
+
+        TypeList[ParameterCount] =
+            MtProcessTypeOp (ParamTypeOp);
+
+        return (1);
+    }
 
     while (ParamTypeOp)
     {
